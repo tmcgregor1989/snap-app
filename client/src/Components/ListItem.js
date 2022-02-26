9632928a843017e0acd73be5f6fd6976a61aafa0
tmcgregor1989 <tmcgregor1989@gmail.com>
2022-02-26 12:13:16 +0000

    high score ListItem started

---
+++ client/src/Components/ListItem.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react';
+
+const ListItem = ({player, deletePlayer}) => {
+
+
+    return (
+        <div className='individual-player'>
+            <p>{player.name}</p>
+            <p>{player.score}</p>
+            <button onClick={()=>deletePlayer(player._id)}>Remove Player from Scoreboard becuase they are a filthy cheat</button>
+        </div>
+    )
+}
+
+default export ListItem;
