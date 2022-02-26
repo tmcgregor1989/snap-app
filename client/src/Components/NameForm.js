1ad3b5844821d2a1eb82b05e8b8742771c28ff6b
Mattbusby <95364588+Mattbusby@users.noreply.github.com>
2022-02-26 12:49:18 +0000

    first Matt commit

--- client/src/Components/NameForm.js
+++ client/src/Components/NameForm.js
@@ -1,2 +1,49 @@
 //2 x text input fields, placeholdered with "player1", "player2".
 // START GAME button that pushes those names
+
+import {useState} from "react";
+import {postPlayers} from "./GamesService";
+// NEED TO UPDATE FILE THAT WILL HAVE FUNCTION IN ABOVE LINE
+
+// BELOW IS THE POSTPLAYERS FUNCTION:
+// export const postPlayers = (payload) => {
+//     return fetch(baseURL, {
+//         method: 'POST',
+//         body: JSON.stringify(payload),
+//         headers: { 'Content-Type': 'application/json'}
+//     })
+//     .then(res => res.json())
+// }
+
+
+const NameForm = ({addPlayers}) =>{
+    const [formData, setFormData] = useState({});
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        formData["player1name"] = formData.player1name;
+        formData["player2name"] = formData.player2name;
+        postPlayers(formData).then((data) => {
+            addNames(data);
+        });
+    }
+
+    const onChange = (e) => {
+        formData[e.target.id] = e.target.value;
+        setFormData(formData);
+    }
+
+    return (
+        <form className="" onSubmit={handleSubmit} method="post">
+            <label htmlFor="player1name">Player 1 Name:</label>
+            <input  onChange={onChange}type="text" id="player1name" v-model="player1name" required/>
+
+            <label htmlFor="player2name">Player 2 Name:</label>
+            <input  onChange={onChange}type="player2name" id="player2name"  required/>
+
+            <input type="submit" value="Save" id="save"/>
+
+        </form>
+    )
+}
+export default NameForm;
--- client/src/Components/Player1Hand.js
+++
@@ -1,3 +0,0 @@
-// player 1 hand[] length displayed numberically
-
-// taken from SnapPool.js
