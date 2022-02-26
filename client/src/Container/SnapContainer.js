0beeab249a4b3b026b4e92c860fba9d5f0112f11
dazzle-tactics <90906037+dazzle-tactics@users.noreply.github.com>
2022-02-26 12:07:01 +0000

    x

--- client/src/Container/SnapContainer.js
+++ client/src/Container/SnapContainer.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from "react";
 
 
-const DeckContainer = () => {
+const SnapContainer = () => {
 
     const [deck, setDeck] = useState([])
+    const [hand1, setHand1] = useState([])
+    const [hand2, setHand2] = useState([])
 
     useEffect(() => {
         getDeck();
@@ -12,12 +14,31 @@ const DeckContainer = () => {
     const getDeck = function(){
         fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=52')
         .then(res => res.json())
-        .then(data => setDeck(data.cards))
+        .then(data => console.log(data))
+            // setDeck(data.cards))
     }
 
-    return
+    const dealDeck = function(deck){
+        // let hand1 = deck
+        // let hand2 = deck
+        // // hand1 = Array.from(new Set(deck))
+        // // hand2 = Array.from(new Set(deck))
+        // // hand1.splice(0, 26)
+        // // hand2.splice(26, 26)
+        // // deck = []
+        // setHand1(hand1)
+        // setHand2(hand2)
+        // console.log(hand1)
+        // console.log(hand2)
+        console.log(deck)
+    }
+
+    return(
         <div id="container">
+            <button onClick={dealDeck} deck={deck}>Deal</button>
         </div>
+    )
+
 }
 
-export default DeckContainer;
+export default SnapContainer;
