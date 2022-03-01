96590968e6695ed49665f47b9eed277a7ac2631b
Mattbusby <95364588+Mattbusby@users.noreply.github.com>
2022-03-01 23:24:54 +0000

    renamed class -> className to remove error in Chrome console

--- client/src/Components/EndGame.js
+++ client/src/Components/EndGame.js
@@ -1,30 +1,29 @@
 import React from 'react';
+import './EndGame.css';
 
 
 const EndGame = ({score1, score2, selectedPlayer1, selectedPlayer2, winner, setWinner, setGameEnded, setGameState}) => {
+    let winner1 = selectedPlayer1
+    let winner2 = selectedPlayer2
+    if (score1 > score2) {
+    setWinner(winner1)
+    }
+    else{
+        setWinner(winner2)
+    }
 
-        
-        let winner1 = selectedPlayer1
-        let winner2 = selectedPlayer2
-        if (score1 > score2) {
-        setWinner(winner1)
-        }else{
-            setWinner(winner2)
-        }
-
-        const handleClick = () =>{
-            setGameEnded(false)
-        }
+    const handleClick = function(){
+        setGameEnded(false)
+        document.location.reload()
+    }
 
 
     return (
-
         <div className="gameover-screen">
         <h3>Game Over</h3>
         <p>{winner.name} wins!!!</p>
-        <button onClick={handleClick}>Rematch?</button>
+        <button onClick={handleClick}>Another Round?</button>
         </div>
-
     )
 }
 
